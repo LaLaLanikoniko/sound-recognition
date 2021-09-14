08d1a3d122b26d098e6d724b4f246d268047c993
LaLaLa <math.uta.e@outlook.jp>
2021-09-14 18:01:49 +0900

    Fix policy about input and output file path.

--- sound_recognition/main.py
+++ sound_recognition/main.py
@@ -7,29 +7,41 @@ from preprocessor import preprocess_wave
 from recognizer import recognize_speech
 from writer import write
 
-SOURCE_DIR = r"B:\SpeechToText"
-TMP_DIR = os.sep.join([SOURCE_DIR, "tmp"])
-
 
 def main():
-    file_name = sys.argv[1]
-    audio_file_path = os.sep.join([SOURCE_DIR, file_name])
+    current_dir = os.getcwd()
+    tmp_dir = os.sep.join([current_dir, "tmp"])
+    audio_file_path = sys.argv[1]
     # init tmp folder
-    shutil.rmtree(TMP_DIR)
-    os.mkdir(TMP_DIR)
+    if os.path.isfile(tmp_dir):
+        shutil.rmtree(tmp_dir)
+    os.mkdir(tmp_dir)
 
     num_cut, sample_rate_hertz, audio_channel_count = preprocess_wave(
-        audio_file_path, 45, TMP_DIR
+        audio_file_path, 45, tmp_dir
     )
-    while len(os.listdir(TMP_DIR)) < num_cut:
+    while len(os.listdir(tmp_dir)) < num_cut:
         pass
 
-    audio_files = glob.glob(os.sep.join([TMP_DIR, "*"]))
+    audio_files = glob.glob(os.sep.join([tmp_dir, "*"]))
     recognition_result = recognize_speech(
         audio_files, sample_rate_hertz, audio_channel_count
     )
-    srt_file_path = os.sep.join([SOURCE_DIR, os.path.splitext(file_name)[0] + ".srt"])
+    shutil.rmtree(tmp_dir)
+
+    srt_file_path = create_srt_file_path(audio_file_path, current_dir)
     write(recognition_result, srt_file_path)
+
+
+def create_srt_file_path(audio_file_path, current_dir):
+    base_name = os.path.splitext(os.path.basename(audio_file_path))[0]
+    srt_file_path = os.sep.join([current_dir, base_name + ".srt"])
+
+    while os.path.isfile(srt_file_path):
+        base_name += "_コピー"
+        srt_file_path = os.sep.join([current_dir, base_name + ".srt"])
+
+    return srt_file_path
 
 
 main()
